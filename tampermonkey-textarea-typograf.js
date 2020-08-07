fafe5d0507fe632f4913b8bac6ef5e6866cbb8f7
Gleb Kemarsky <gleb.kemarsky@gmail.com>
2020-08-07 21:20:49 +0200

    0.2. RegExp fot russian words

--- tampermonkey-textarea-typograf.js
+++ tampermonkey-textarea-typograf.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Textarea Typograf
 // @namespace    https://github.com/glebkema/tampermonkey-textarea-typograf
-// @version      0.1
+// @version      0.2
 // @description  Replaces hyphens and quotation marks. Works only in the <textarea>. If you select a part of the text, only that part will be processed.
 // @author       Gleb Kemarsky
 // @grant        none
@@ -26,14 +26,31 @@
         // console.info('Start editing a non-empty textarea before calling the script');
     }
 
+    // console.log(typograf('Еще она - не очень еще "еще" любила (Еще и еще). Еще вот еще, еще. И еще'));
+
     function typograf(text) {
         if (text) {
+            // dash
             text = text.replace(/ - /gi, ' — ');
+
+            // quotes
             text = text.replace(/^"/gi, '«');
             text = text.replace(/"$/gi, '»');
             text = text.replace(/([\(\s])"/gi, '$1«');
             text = text.replace(/"([.,;\s\)])/gi, '»$1');
+
+            // words
+            text = replace_words(text, 'еще', 'ещё');
+            text = replace_words(text, 'Еще', 'Ещё');
+            text = replace_words(text, 'ее', 'её');
+            text = replace_words(text, 'Ее', 'Её');
         }
         return text;
+    }
+
+    function replace_words(text, find, replace) {
+        // \b doesn't work for russian words
+        var regex = new RegExp('(?=[^А-Яа-яЁё]|^)(' + find + ')(?=[^а-яё]|$)', 'g');
+        return text.replace(regex, replace);
     }
 })();
